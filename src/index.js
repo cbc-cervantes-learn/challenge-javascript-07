7620b8e14f006efa0340207094741ef3d8e86aac
Cristian Bello Cervantes <cristiancervantes.cbc@gmail.com>
2020-06-15 21:20:56 -0500

    Add challenge solution

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@
  */
 
 const hammingDistance = (a, b) => {
-    
+  let distance = 0;
+  if (a.length === b.length){
+    let length = a.length - 1;
+    while(length >= 0){
+      if (a[length] !== b[length]){
+        distance++;
+      }
+     length--;
+    }
+  }else{
+    throw new Error("Strings must have the same length")
   }
-  
+  return distance;
+}
   module.exports = hammingDistance;
